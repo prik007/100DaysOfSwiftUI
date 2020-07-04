28f594f0c9b2daf50ad0ec44ba82c2589ba1bf13
Khasnobis, Pritha (US - Bengaluru) <pkhasnobis@deloitte.com>
2020-07-04 18:54:01 +0530

    Initial Commit

---
+++ 100DaysOfSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  100DaysOfSwiftUI
+//
+//  Created by Khasnobis, Pritha on 04/07/20.
+//  Copyright © 2020 Deloitte. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
