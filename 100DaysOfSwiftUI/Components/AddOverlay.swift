e90926c74ed518999fa0ca8ee5deab5144d32e96
Khasnobis, Pritha (US - Bengaluru) <pkhasnobis@deloitte.com>
2020-07-04 23:15:01 +0530

    Formatted texts

---
+++ 100DaysOfSwiftUI/Components/AddOverlay.swift
@@ -0,0 +1,26 @@
+//
+//  AddOverlay.swift
+//  100DaysOfSwiftUI
+//
+//  Created by Khasnobis, Pritha on 04/07/20.
+//  Copyright © 2020 Deloitte. All rights reserved.
+//
+
+import SwiftUI
+
+struct AddOverlay<Content: View, Overlay: View>: View {
+    
+    let content: Content
+    let overlay: Overlay
+    
+    internal init(_ content: Content, _ overlay: Overlay) {
+        self.content = content
+        self.overlay = overlay
+    }
+    
+    var body: some View {
+        content
+            .overlay(overlay)
+            .mask(content)
+    }
+}
